57651e9a224cda00570b9d7a1983c6cad50219c1
Simone Pagan Griso <spagangriso@lbl.gov>
2022-06-22 22:09:44 -0700

    updated script to parse all frontiers and get names automatically

--- parse-twiki-html.py
+++ parse-twiki-html.py
@@ -2,33 +2,17 @@ from html.parser import HTMLParser
 import argparse
 from os.path import exists
 import re
-
-#Dictionat of section id names to be searched for in the HTML and their convenience label
-listOfFrontiers={
-    "papers_of_general_interest_to_this_frontier": "EF0",
-    "ef01higgs_boson_properties_and_couplings": "EF01",
-    "ef02higgs_boson_as_a_portal_to_new_physics": "EF02",
-    "ef03heavy_flavor_and_top_quark_physics": "EF03",
-    "ef04electroweak_precision_physics_and_constraining_new_physics": "EF04",
-    "ef05precision_qcd": "EF05",
-    "ef06hadronic_struture_and_forward_qcd": "EF06",
-    "ef07heavy_ions": "EF07",
-    "ef08beyond_the_standard_modelmodel-specific_explorations": "EF08",
-    "ef09beyond_the_standard_modelmore_general_explorations": "EF09",
-    "ef10beyond_the_standard_modeldark_matter_at_colliders": "EF10"
-}
+import csv
 
 #output record
-# - frontier (one of the convenience labels defined in listOfFrontiers)
-# - arXiv (arXiv number)
-# - title
-# - also: list of other frontiers the paper appears in
 currentRecord={
     'frontier':'', 
     'arxiv': '', 
-    'title': '',
     'also': ''
 }
+
+#need to keep track of frontier from file name unfortunately
+currentFrontierFile=''
 
 #output list of dictionary of the format of currentRecord above
 outputPapers=[]
@@ -45,10 +29,20 @@ def getFrontierLabelFromTag(tag, attrs):
     for att in attrs:
         if (att[0] == 'id'):
             #check if we're starting a new relevant frontier
-            for relevantSection in listOfFrontiers.keys():
-                if att[1] == relevantSection:
-                    validFrontier=listOfFrontiers[relevantSection]
-                    break
+            if att[1] == 'papers_of_general_interest_to_this_frontier':
+                #frontier depends on file name
+                validFrontier = currentFrontierFile+'00'
+            else:
+                if cmdArgs.debug > 15:
+                    print('Searching "({}\d\d).+" in string: {}'.format(currentFrontierFile.lower(), att[1]))
+
+                searchResult = re.search('({}\d\d).+'.format(currentFrontierFile.lower()), att[1])
+                if searchResult:
+                    validFrontier = searchResult.group(1).upper()
+#            for relevantSection in listOfFrontiers.keys():
+#                if att[1] == relevantSection:
+#                    validFrontier=listOfFrontiers[relevantSection]
+#                    break
 
     #return result
     return validFrontier
@@ -74,12 +68,11 @@ class paperHTMLParser(HTMLParser):
         #append output record when finding a closing "li" tag and we have a valid arXiv number
         if tag == 'li':
             if currentRecord['arxiv']:
-                outputPapers.append(currentRecord)
+                outputPapers.append(currentRecord.copy())
                 if cmdArgs.debug > 3:
                     print('- New record found: ', currentRecord)
                 #empty field, but keep frontier
                 currentRecord['arxiv'] = ''
-                currentRecord['title'] = ''
                 currentRecord['also'] = ''
 
         #check if we need to close active frontier (close of div tag opened with class 'level4', first after h4 tag)
@@ -96,19 +89,7 @@ class paperHTMLParser(HTMLParser):
         #only analyze data if in an active frontier
         if not currentRecord['frontier']:
             return
-
-        # simply store potential title found in data, if we're parsing a valid frontier
-        #check if data might contain a title in double quotes
-        searchResult = re.search('\u0022(.+)\u0022', data, re.UNICODE)
-        #searchResult = re.search(u'“(.+)“,', data, re.UNICODE)
-        #searchResult = re.search('“(.+)“,', data)
-        if searchResult:
-            #found new potential title
-            currentRecord['title'] = searchResult.group(1)
-            if cmdArgs.debug > 5:
-                print('Found title: ', currentRecord['title'])
-            return
-            
+         
         #check if data might contain the arXiv number, if so assume title came before and finish the record
         searchResult = re.search('arXiv:(\S+)', data)
         if searchResult:
@@ -135,6 +116,7 @@ if __name__ == '__main__':
 
     cmdParser = argparse.ArgumentParser(description='Parse twiki HTML source for contributed papers')
     cmdParser.add_argument('input_files', metavar='inputFile', type=str, nargs='+', help='Input HTML source file(s)')
+    cmdParser.add_argument('--output_file', dest='output_file', type=str, default='sm21-papers.csv', help='Output CSV file')
     cmdParser.add_argument('--debug', dest='debug', type=int, default=0, help='Enable debug printout')
     cmdArgs = cmdParser.parse_args()
 
@@ -150,10 +132,27 @@ if __name__ == '__main__':
 
         # open and parse input HTML file
         with open(input_file, encoding="utf-8") as fHTML:
+            currentFrontierFile = input_file.split('_')[6].split('.')[0].upper()
             linesHTML = fHTML.read()
 
         phParser = paperHTMLParser()
         phParser.feed(linesHTML)
 
     #print statistics
-    
+    stat={}
+    groups = sorted(list(set([rec['frontier'] for rec in outputPapers])))
+    print('Number of papers per frontier:')
+    for g in groups:
+        #count occurrences in outputPapers
+        occ = len([rec for rec in outputPapers if rec['frontier'] == g])
+        print('{}: {}'.format(g, occ))
+        stat[g] = occ
+    print('Total:', sum([stat[g] for g in groups]))
+
+    #save to file
+    with open(cmdArgs.output_file, 'w') as output_file:
+        csvWriter = csv.DictWriter(output_file, fieldnames=outputPapers[0].keys())
+        csvWriter.writeheader()
+        for data in outputPapers:
+            csvWriter.writerow(data)
+    print('Output saved to CSV file: ', cmdArgs.output_file)
