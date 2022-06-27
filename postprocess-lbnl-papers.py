eb81a41a2822c53acc25d06b81ae5cf373229f6e
Simone Pagan Griso <spagangriso@lbl.gov>
2022-06-27 11:00:25 -0700

    remove useless info

--- postprocess-lbnl-papers.py
+++ postprocess-lbnl-papers.py
@@ -20,12 +20,13 @@ if __name__ == '__main__':
     papers = pd.concat(pd.read_csv(f, dtype={'arxiv': str, 'frontier': str, 'also': str}) for f in cmdArgs.input_files)
 
     #now make unique (in arxiv number) rows and make an "OR" of the 'LBL direct role?' column
-    arxiv_list = papers['arxiv'].unique()
-
     paper_merge = {'arxiv': 'first', 'frontier': 'first', 'title':'first', 'LBL direct role?': 'any','lbl_authors': 'first', 'lbl_inst': 'first'}
     out_papers = papers.groupby(papers['arxiv']).aggregate(paper_merge)
 
-    print(out_papers)
+    #retrieve statistics per frontier
+    
+
+
     print('Saving selected papers to: ', cmdArgs.output_file)
     out_papers.to_csv(cmdArgs.output_file,index=False)
-     
+    
